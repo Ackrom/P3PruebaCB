46642d0aca0d3ddf2016ef90e49505c2db05b4fa
Ackrom <marco.at720@gmail.com>
2018-06-29 00:46:06 -0430

    paso 5 y 6

--- include/AyudaFoneticaSP.hpp
+++ include/AyudaFoneticaSP.hpp
@@ -19,7 +19,9 @@ class AyudaFoneticaSP
         //cada palabra a buscar arreglada foneticamente
         string *palabrasABuscarArregladas;
 
-
+        /*
+        * Primer paso
+        **/
         void guardarMayorImportancia()
         {
             int i;
@@ -27,31 +29,78 @@ class AyudaFoneticaSP
                 letraDeMayorImportancia[i] = palabrasABuscar[i].at(0);
             }
         }
-//        void eliminarCaracteres(string palabra)
-//        {
-//            int i;
-//            for(i=0; i< palabra.length() ; i++){
-//                if(palabra.at(i) == 'A' ){
-//
-//                }
-//            }
-//        }
+        /*
+        * Segundo paso
+        **/
+        void eliminarCaracteres(string palabra)
+        {
+            int i;
+            for(i=0; i< palabra.length() ; i++){
+                if(palabra.at(i) == 'A' ){
+
+                }
+            }
+        }
+        /*
+        * tercer paso
+        **/
         void reemplazarLetrasPorNumeros();
+        /*
+        * Cuarto paso
+        **/
         void unirValoresNumericosConLetras();
-        void letraYtresPrimerosNumeros();
+
+
+
+
         void eliminarRepetidos();
 
     public:
-        /** Default constructor */
-        AyudaFoneticaSP(int filas, int columnas, char** matriz, int cantPalabrasABuscar, string* palabrasABuscar){
-            this->filas = filas;
-            this->columnas = columnas;
-            this->matriz = matriz;
-            this->cantPalabrasABuscar = cantPalabrasABuscar;
-            this->palabrasABuscar = palabrasABuscar;
+        /*
+        * Quinto paso
+        **/
+        void unirNumerosConsecutivos(string palabra){
+            string apuntador;
+            string salida = "";
+            string aux;
+            apuntador = palabra.at(0);
+            salida= salida + (string) apuntador;
+            int i;
+            for(i=1; i< palabra.length() ; i++){
+                aux = palabra.at(i);
+                if(apuntador != aux ){
+                    apuntador = palabra.at(i);
+                    salida.insert(salida.end(), palabra.at(i));
+                }
+            }
+            cout << salida;
         }
+        /*
+        * Sexto paso
+        **/
+        void letraYPrimerosNumeros(string palabra){
+            if(palabra.length()>=4){
+                palabra = palabra.substr(0,4);
+            }else{
+                int dif = 4 - palabra.length();
+                int i;
+                for(i=0;i<dif;i++){
+                    palabra = palabra + "0";
+                }
+            }
+            cout << palabra;
+        }
+        /** Default constructor */
+//        AyudaFoneticaSP(int filas, int columnas, char** matriz, int cantPalabrasABuscar, string* palabrasABuscar){
+//            this->filas = filas;
+//            this->columnas = columnas;
+//            this->matriz = matriz;
+//            this->cantPalabrasABuscar = cantPalabrasABuscar;
+//            this->palabrasABuscar = palabrasABuscar;
+//        }
+//        AyudaFoneticaSP(){}
         /** Default destructor */
-        virtual ~AyudaFoneticaSP();
+//        virtual ~AyudaFoneticaSP();
 
         void realizarBusqueda();
 
--- main.cpp
+++ main.cpp
@@ -6,24 +6,28 @@ using namespace std;
 
 int main()
 {
-    int fila, columna;
-    int i;
-    cin>> fila >>columna;
 
-    char** matriz = new char* [fila];
-    for(i=0;i<fila;i++){
-        matriz[i] = new char[columna];
-    }
-
-    string* h = new string[fila];
-    for(i=0;i<fila;i++){
-        cin >>h[i];
-    }
-    cout<<endl;
-    h[0];
-    for(i=0;i<fila;i++){
-        cout<< h[i] <<"  ";
-    }
+    AyudaFoneticaSP SP;
+    //SP.unirNumerosConsecutivos("p222233322223333");
+    SP.letraYPrimerosNumeros("ff");
+//    int fila, columna;
+//    int i;
+//    cin>> fila >>columna;
+//
+//    char** matriz = new char* [fila];
+//    for(i=0;i<fila;i++){
+//        matriz[i] = new char[columna];
+//    }
+//
+//    string* h = new string[fila];
+//    for(i=0;i<fila;i++){
+//        cin >>h[i];
+//    }
+//    cout<<endl;
+//    h[0];
+//    for(i=0;i<fila;i++){
+//        cout<< h[i] <<"  ";
+//    }
 
 
     //AyudaFoneticaSP SP = new AyudaFoneticaSP(fila, columna, matriz);
